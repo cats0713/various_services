3ae67fa170c50bd9f700aecab98b97bf9671d0a9
cats0713 <cats0713@naver.com>
2022-01-21 21:49:24 +0900

    경고창 띄우기,  ui 수정

--- decimal_number/decimal_number.js
+++ decimal_number/decimal_number.js
@@ -1,144 +1,162 @@
 let checkBtn = document.querySelector("#checkBtn");
-let cancelBtn =document.querySelector("#cancelBtn");
+let cancelBtn = document.querySelector("#cancelBtn");
+let errorXBtn = document.querySelector("#errorXBtn");
 
-checkBtn.addEventListener("click", function(){
+checkBtn.addEventListener("click", function () {
   let userInput = document.querySelector("#userInput").value;
-  let userResult = document.querySelectorAll(".resultBox>input");
+  let userResult = document.querySelectorAll(".resultInput>input");
   let i = 0;
   let binaryNum = 0;
   let octalNum = 0;
   let decimalNum = Number(userInput);
   let hexadecimalNum = 0;
 
-  function binaryFunc(decimalNum){
-    let binaryNumArr = [];
-    let binaryNum = '';
-    let numberCal = decimalNum;
-    let i = 0;
+  if (!isNaN(decimalNum)) {
 
-    while(true){ //계산
-      binaryNumArr.unshift(Math.floor(numberCal % 2));
-      numberCal = Math.floor(numberCal / 2);
+    function binaryFunc(decimalNum) {
+      let binaryNumArr = [];
+      let binaryNum = '';
+      let numberCal = decimalNum;
+      let i = 0;
 
-      if( numberCal == 1 ){
-        binaryNumArr.unshift(numberCal);
-        break;
-      }else if( numberCal == 0){
-        break;
+      while (true) { //계산
+        binaryNumArr.unshift(Math.floor(numberCal % 2));
+        numberCal = Math.floor(numberCal / 2);
+
+        if (numberCal == 1) {
+          binaryNumArr.unshift(numberCal);
+          break;
+        } else if (numberCal == 0) {
+          break;
+        }
       }
+
+      while (i < binaryNumArr.length) { //배열을 문자열로 바꾸는 과정
+        binaryNum += binaryNumArr[i]
+        i++;
+      }
+
+      return binaryNum;
     }
 
-    while(i < binaryNumArr.length){ //배열을 문자열로 바꾸는 과정
-      binaryNum +=  binaryNumArr[i]
+    function octalFunc(decimalNum) {
+      let octalNumArr = [];
+      let octalNum = '';
+      let numberCal = decimalNum;
+      let i = 0;
+
+      while (true) { //계산
+        octalNumArr.unshift(Math.floor(numberCal % 8));
+        numberCal = Math.floor(numberCal / 8);
+
+        //console.log(octalNumArr);
+        //console.log(numberCal);
+
+        if (numberCal < 8) {
+          if(numberCal==0){
+            octalNumArr[0] = 0;
+          }else{
+            octalNumArr.unshift(numberCal);
+          }
+          break;
+        }
+      }
+
+      while (i < octalNumArr.length) { //배열을 문자열로 바꾸는 과정
+        octalNum += octalNumArr[i]
+        i++;
+      }
+
+      return octalNum;
+    }
+
+    function hexadecimalFunc(decimalNum) {
+      let hexadecimalNumArr = [];
+      let hexadecimalNum = '';
+      let numberCal = decimalNum;
+      let i = 0;
+
+      while (true) {
+        hexadecimalNumArr.unshift(Math.floor(numberCal % 16));
+        numberCal = Math.floor(numberCal / 16);
+
+        if (numberCal < 16) {
+          if(numberCal==0){
+            hexadecimalNumArr[0] = 0;
+          }else{
+            hexadecimalNumArr.unshift(numberCal);
+          }
+          break;
+        }
+      }
+
+      while (i < hexadecimalNumArr.length) {
+        switch (hexadecimalNumArr[i]) {
+          case 10: hexadecimalNumArr[i] = 'A'; break;
+          case 11: hexadecimalNumArr[i] = 'B'; break;
+          case 12: hexadecimalNumArr[i] = 'C'; break;
+          case 13: hexadecimalNumArr[i] = 'D'; break;
+          case 14: hexadecimalNumArr[i] = 'E'; break;
+          case 15: hexadecimalNumArr[i] = 'F'; break;
+        }
+        i++;
+      }
+      i = 0;
+      while (i < hexadecimalNumArr.length) {
+
+        hexadecimalNum += hexadecimalNumArr[i];
+        i++;
+      }
+      return hexadecimalNum;
+    }
+
+    binaryNum = binaryFunc(decimalNum); // 바이너리로 바꿈
+    octalNum = octalFunc(decimalNum); // 8진수
+    hexadecimalNum = hexadecimalFunc(decimalNum); // 16진수
+
+    while (i < 4) { //출력
+      switch (i) {
+        case 0:
+          userResult[i].value = binaryNum;
+          break;
+
+        case 1:
+          userResult[i].value = octalNum;
+          break;
+
+        case 2:
+          userResult[i].value = decimalNum;
+          break;
+
+        case 3:
+          userResult[i].value = hexadecimalNum;
+          break;
+      }
       i++;
     }
 
-    return binaryNum;
+
+  } else {
+    let errorBox = document.querySelector("#errorBox");
+    errorBox.style.display = "block";
   }
-
-  function octalFunc(decimalNum){
-    let octalNumArr = [];
-    let octalNum = '';
-    let numberCal = decimalNum;
-    let i = 0;
-
-    while(true){ //계산
-      octalNumArr.unshift(Math.floor(numberCal % 8));
-      numberCal = Math.floor(numberCal / 8);
-
-      console.log(octalNumArr);
-      console.log(numberCal);
-
-      if( numberCal < 8 ){
-        octalNumArr.unshift(numberCal);
-        break;
-      }else if( numberCal == 0){
-        break;
-      }
-    }
-
-    while(i < octalNumArr.length){ //배열을 문자열로 바꾸는 과정
-      octalNum +=  octalNumArr[i]
-      i++;
-    }
-
-    return octalNum;
-  } 
-  
-  function hexadecimalFunc(decimalNum){
-    let hexadecimalNumArr = [];
-    let hexadecimalNum = '';
-    let numberCal = decimalNum;
-    let i = 0;
-
-    while(true){
-      hexadecimalNumArr.unshift(Math.floor(numberCal % 16));
-      numberCal = Math.floor(numberCal / 16);
-    
-      if( numberCal < 16 ){
-        hexadecimalNumArr.unshift(numberCal);
-        break;
-      }else if( numberCal == 0){
-        break;
-      }
-    }
-
-    while(i < hexadecimalNumArr.length){
-      switch (hexadecimalNumArr[i]){
-        case 10 : hexadecimalNumArr[i] = 'A'; break;
-        case 11 : hexadecimalNumArr[i] = 'B'; break; 
-        case 12 : hexadecimalNumArr[i] = 'C'; break; 
-        case 13 : hexadecimalNumArr[i] = 'D'; break;
-        case 14 : hexadecimalNumArr[i] = 'E'; break;
-        case 15 : hexadecimalNumArr[i] = 'F'; break;
-      }
-      i++;
-    }
-    i = 0;
-    while(i < hexadecimalNumArr.length){
-      
-      hexadecimalNum +=  hexadecimalNumArr[i];
-    i++;
-    }
-    return hexadecimalNum;
-  } 
-
-  binaryNum = binaryFunc(decimalNum); // 바이너리로 바꿈
-  octalNum = octalFunc(decimalNum); // 8진수
-  hexadecimalNum = hexadecimalFunc(decimalNum); // 16진수
-  
-  while(i < 4){ //출력
-    switch(i){
-      case 0: 
-      userResult[i].value = binaryNum;
-      break;
-
-      case 1: 
-      userResult[i].value = octalNum; 
-      break;
-
-      case 2: 
-      userResult[i].value = decimalNum;  
-      break;
-
-      case 3: 
-      userResult[i].value = hexadecimalNum;  
-      break;
-    }
-    i++;
-  }
-  
 
 });
 
-cancelBtn.addEventListener("click", function(){
+errorXBtn.addEventListener("click", function(){
+  let errorBox = document.querySelector("#errorBox");
+  errorBox.style.display = "none";
+});
+
+cancelBtn.addEventListener("click", function () {
   let userInput = document.querySelector("#userInput");
-  let userResult = document.querySelectorAll(".resultBox>input");
+  let userResult = document.querySelectorAll(".resultInput>input");
   let i = 0;
 
   userInput.value = '';
-  while(i<4){
+  while (i < 4) {
     userResult[i].value = 0;
     i++;
   }
 });
+
