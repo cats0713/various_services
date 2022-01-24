e0ebe332c74e35593744d3bb8e9ce383d57a60e2
cats0713 <cats0713@naver.com>
2022-01-24 09:39:32 +0900

    객체를 활용한 행렬계산기

--- matrix_calculator/matrix_calculator.js
+++ matrix_calculator/matrix_calculator.js
@@ -1,7 +1,18 @@
 /* matrtix_calculator  by ahyeon*/
+let AAA = document.querySelector(".matirxArr>fieldset");
+let BBB = document.querySelector(".matirxArr");
+function ArrInputBox(arrX,arrY,arrIdName){
+  this.arrX = arrX;
+  this.arrY = arrY;
+  this.arrIdName = arrIdName;
+  this.arrInput = `<input id="${arrIdName}" class="${arrIdName}" type="number"></input>`;
+  this.arrDisplayFunc = function(){
+    BBB.stlye.display = "block";
+    AAA.innerHTML = this.arrInput;
 
-function ArrInputBox(arrWidth, arrHeight){
-  this.width = arrWidth;
-  this.height = arrHeight;
+  };
 
 }
+
+let arrA = new ArrInputBox(2,2,'arrA');
+arrA.arrDisplayFunc();
