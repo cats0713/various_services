f8e315fe44c6c38edca41b0844e3b5db541ccf4c
cats0713 <cats0713@naver.com>
2022-02-08 09:47:12 +0900

    웹사이트

--- wabsite_festival/wabsite_festival.js
+++ wabsite_festival/wabsite_festival.js
@@ -1,4 +1,26 @@
-window.onload = function(){
-  
+window.onload = () => {
+  const textArray = `다시찾고 싶은 마을,
+  새로운 에너지를 담는 마을
+  알프스마을로 여러분을 초대합니다.`;
+  const disText = textArray.split("");
+  const mainText = document.querySelector("#mainText");
+
+  let displaylater = () => { //closer
+    let i = 0;
+    let display = () => {
+      if(i >= 0 && i < disText.length){
+        if(disText[i] == '\n'){
+          mainText.innerHTML += '<br>';
+        }else{
+        mainText.innerHTML += disText[i];
+        }
+      }
+        i++;
+    }
+    return display;
+  }
+
+  let userdisText = displaylater();
+  setInterval(userdisText, 100);
 
 }
