5d8bff3c304593dcb8b9f59eafc4e34918b46f3b
cats0713 <cats0713@naver.com>
2022-02-09 17:13:40 +0900

    웹 사이트 비디오

--- Compiled-HTML/main/main.js
+++ Compiled-HTML/main/main.js
@@ -2,30 +2,30 @@ window.onload = () => {
   let navBtn = document.querySelector("#navBtn");
   let navXBtn = document.querySelector("#navXBtn");
   let navListBtn = document.querySelector("#navListBtn");
-  let blackBackgruond =document.querySelector("#blackBackgruond");
+  let blackBackgruond = document.querySelector("#blackBackgruond");
   
-  // const textArray = `새로운 에너지를 담는 마을
-  // 알프스마을로 여러분을 초대합니다.`;
-  // const disText = textArray.split("");
-  // const mainText = document.querySelector("#mainText");
+  const textArray = `새로운 에너지를 담는 마을
+  알프스마을로 여러분을 초대합니다.`;
+  const TitleText = textArray.split("");
+  const mainText = document.querySelector("#mainTitle");
 
-  // let displaylater = () => { //closeur
-  //   let i = 0;
-  //   let display = () => {
-  //     if(i >= 0 && i < disText.length){
-  //       if(disText[i] == '\n'){
-  //         mainText.innerHTML += '<br>';
-  //       }else{
-  //       mainText.innerHTML += disText[i];
-  //       }
-  //     }
-  //       i++;
-  //   }
-  //   return display;
-  // }
+  let displaylater = () => { //closeur
+    let i = 0;
+    let display = () => {
+      if(i >= 0 && i < TitleText.length){
+        if(TitleText[i] == '\n'){
+          mainText.innerHTML += '<br>';
+        }else{
+        mainText.innerHTML += TitleText[i];
+        }
+      }
+        i++;
+    }
+    return display;
+  }
 
-  // let userdisText = displaylater();
-  // setInterval(userdisText, 100);
+  let userdisText = displaylater();
+  setInterval(userdisText, 100);
 
   //이벤트 리스너
   navBtn.addEventListener("click",function(){
