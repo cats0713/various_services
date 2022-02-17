aa59744f51db8a9afe2c2df1422357f5c9f9a3b6
cats0713 <cats0713@naver.com>
2022-02-17 14:24:24 +0900

    web

--- Compiled-HTML/iceFestival/iceFestival.js
+++ Compiled-HTML/iceFestival/iceFestival.js
@@ -1,27 +1,30 @@
 window.onload = () => {
-  let introBoxImg = document.querySelector("#introBox img");
-  const mainArray = ['./IMG/mainimg/main_1.png','./IMG/mainimg/main_2.jpeg','./IMG/mainimg/main_3.png']; 
-  const mainTextArr = [`알프스 겨울왕국의<br>크고 아름다운 얼음분수`,`짜릿하고 즐거운 썰매로<br>즐거운 추억을 남겨보세요`,`겨울에만 볼수있는 장관<br>아름다운 얼음 조각`]
   const snowBox = document.querySelector("#snowBox");
   const snowBox2 = document.querySelector("#snowBox2");
-  const mainText = document.querySelector("#mainText");
-  let i = 0,j = 0;
+  const introBoxImg = document.querySelector("#introBox img");
+  const introBoxText = document.querySelector("#introBoxText");
 
   //메인 화면 체인지 하는 거
   let changeImg = () => {
-    setTimeout(function(){
-      mainTextArr.push(mainTextArr.shift());
-      mainText.innerHTML = mainTextArr[0];
-      mainArray.push(mainArray.shift());
-      introBoxImg.setAttribute("src",mainArray[0]);
-      changeImg();
-    }, 5000);
-    
+    const mainImgArray = ['./IMG/mainimg/main_1.png','./IMG/mainimg/main_2.jpeg','./IMG/mainimg/main_3.png']; 
+    const mainTextArray = [`알프스 겨울왕국의<br>크고 아름다운 얼음분수`,`짜릿하고 즐거운 썰매로<br>즐거운 추억을 남겨보세요`,`겨울에만 볼수있는 장관<br>아름다운 얼음 조각`];
+    function setTimeImg(){
+      setTimeout(function(){
+        mainTextArray.push(mainTextArray.shift());
+        introBoxText.innerHTML = mainTextArray[0];
+        mainImgArray.push(mainImgArray.shift());
+        introBoxImg.setAttribute("src",mainImgArray[0]);
+        setTimeImg();
+      }, 5000);
+    }
+    setTimeImg();
   } 
   changeImg();
 
+
   //배경화면 눈 내리게 하는거
-  let createSnow = () => {
+  let createSnow = (snowBox) => {
+    let i = 0;
     while(i < 400){
       let randomW = Math.floor(Math.random() * window.innerWidth);
       let randomH = Math.floor(Math.random() * window.innerHeight);
@@ -29,16 +32,17 @@ window.onload = () => {
       i++;
     }
   }
-  let createSnowIntro = () => {
-    while(j < 400){
+  let createSnowIntro = (snowBox2) => {
+    let i = 0;
+    while(i < 400){
       let randomW = Math.floor(Math.random() * window.innerWidth);
       let randomH = Math.floor(Math.random() * window.innerHeight);
       snowBox2.innerHTML += `<div class="snow" style="margin-top: ${randomH}px; margin-left: ${randomW}px"></div>`;
-      j++;
+      i++;
     }
   }
-  createSnow();
-  createSnowIntro();
+  createSnow(snowBox);
+  createSnowIntro(snowBox2);
 
   //지도 열기
   $('#moreBtn').on("click", function () {
@@ -110,18 +114,68 @@ window.onload = () => {
   $('#snowmenBtn').on("click", function () {
     $('#penguinBox').slideToggle('slow');
     $('#displayBox').fadeIn('slow');
-    $('#imgBox').attr("src", "./IMG/displaybox/snowmen.jpeg");
-    $('#imgBox').attr("alt", "눈 조각사진");
+    $('#imgBox').attr("src", "./IMG/displaybox/snowmen2.png");
+    $('#imgBox').attr("alt", "눈 조각");
     $('.textBox').html(`<h1>눈조각</h1>
       <p>커다란 얼음분수가 만들어식은 알프스 겨울왕국!</p>`);
   });
-
+  //짚트랙
+  $('#ziptrack').on("click", function () {
+    $('#penguinBox').slideToggle('slow');
+    $('#displayBox').fadeIn('slow');
+    $('#imgBox').attr("src", "./IMG/displaybox/ziptrack.jpeg");
+    $('#imgBox').attr("alt", "짚트랙");
+    $('.textBox').html(`<h1>짚트랙</h1>
+    <p>커다랙 얼음분수가 만들어놓은 알프스 겨울왕국!</p>`);
+  });
 
   //닫힘 버튼
   $('#closeBtn').on("click", function () {
     $('#displayBox').fadeOut('slow');
     $('#penguinBox').slideToggle('slow');
   });
+
+  let imgBtn = () => {
+    const ziptrackImgArray = ['./IMG/displaybox/ziptrack.jpeg','./IMG/displaybox/ziptrack2.jpeg','./IMG/displaybox/ziptrack3.png'];
+    const foodImgArray = ['./IMG/displaybox/food.jpeg','./IMG/displaybox/food2.jpeg'];
+    const palaceImgArray = ['./IMG/displaybox/palace.png','./IMG/displaybox/palace2.jpeg'];
+    const snowmenImgArray = ['./IMG/displaybox/snowmen2.png','./IMG/displaybox/snowmen.jpeg'];
+    const  icecaveImgArray = ['./IMG/displaybox/icecave.png','./IMG/displaybox/icecave2.jpeg'];
+    
+    //img right 버튼 
+    $('#imgRBtn').on('click',function(){
+      const nowBox = $('#textBox h1').html();
+      switch(nowBox){
+        case '짚트랙':
+          ziptrackImgArray.push(ziptrackImgArray.shift());
+          $('#imgBox').attr("src", ziptrackImgArray[0]);
+          $('#imgBox').attr("alt", "짚트랙");
+          break;
+        case '음식':
+          foodImgArray.push(foodImgArray.shift());
+          $('#imgBox').attr("src", foodImgArray[0]);
+          $('#imgBox').attr("alt", "음식사진");
+          break;
+        case '얼음조각':
+          palaceImgArray.push(palaceImgArray.shift());
+          $('#imgBox').attr("src", palaceImgArray[0]);
+          $('#imgBox').attr("alt", "얼음조각");
+          break;
+        case '눈조각':
+          snowmenImgArray.push(snowmenImgArray.shift());
+          $('#imgBox').attr("src", snowmenImgArray[0]);
+          $('#imgBox').attr("alt", "눈조각");
+          break;  
+        case '동굴':
+          icecaveImgArray.push(icecaveImgArray.shift());
+          $('#imgBox').attr("src", icecaveImgArray[0]);
+          $('#imgBox').attr("alt", "동굴 안");
+          break;  
+      };
+    });
+  }
+  imgBtn();
+
 
 }
 
