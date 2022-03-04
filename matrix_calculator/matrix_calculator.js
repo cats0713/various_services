a1e58106e38ae948a8871929ddb88f8ac09ecf75
cats0713 <cats0713@naver.com>
2022-03-05 00:35:26 +0900

    행렬계산기

--- matrix_calculator/matrix_calculator.js
+++ matrix_calculator/matrix_calculator.js
@@ -4,16 +4,17 @@ window.onload = function () {
   let matirxArrB = document.querySelector("#matirxArrB");
   let matirxArrR = document.querySelector("#matirxArrR");
 
-  let plusBtn = document.querySelector("#plusBtn");
-  let minusBtn = document.querySelector("#minusBtn");
+  let okABtn = document.querySelector("#okABtn");
+  let ramdomABtn = document.querySelector("#ramdomABtn");
 
-  let xBtn = document.querySelector(".xBtn");
-  let resultBox = document.querySelector("#resultBox");
+  let okBBtn = document.querySelector("#okBBtn");
+  let ramdomBBtn = document.querySelector("#ramdomBBtn");
 
   let addBtn = document.querySelector("#addBtn");
   let miusBtn = document.querySelector("#miusBtn");
   let mulBtn = document.querySelector("#mulBtn");
 
+  let blackBox = document.querySelector(".blackBox");
   let arrcont = 3; //closure 로 바꾸기
 
 
@@ -21,12 +22,12 @@ window.onload = function () {
   //행렬을 생성하는 class
   function ArrInputBox(arrID, matirxArr, arrTapNumber) {
     this.arrID = arrID;
-    this.arrDisplayFunc = function (arrXY) {
+    this.arrDisplayFunc = function (arrX,arrY) {
       let i = 0;
       matirxArr.innerHTML = '';
 
-      while (i < arrXY * arrXY) {
-        if ((i + 1) % arrXY == 0) {
+      while (i < arrX * arrY) {
+        if ((i + 1) % arrX == 0) {
           matirxArr.innerHTML += `<input id="${arrID}" class="${arrID}" tabindex="${(i + arrTapNumber) * 10}" type="number" value="0" placeholder="0" required></input>`;
           matirxArr.innerHTML += `<br>`;
         } else {
@@ -39,12 +40,12 @@ window.onload = function () {
   //결과창 행렬 class
   function ArrResultBox(arrID, matirxArr) {
     this.arrID = arrID;
-    this.arrDisplayFunc = function (arrXY) {
+    this.arrDisplayFunc = function (arrX,arrY) {
       let i = 0;
       matirxArr.innerHTML = '';
 
-      while (i < arrXY * arrXY) {
-        if ((i + 1) % arrXY == 0) {
+      while (i < arrX * arrY) {
+        if ((i + 1) % arrX == 0) {
           matirxArr.innerHTML += `<input id="${arrID}" class="${arrID}"  type="number" value="0" readonly></input>`;
           matirxArr.innerHTML += `<br>`;
         } else {
@@ -57,32 +58,42 @@ window.onload = function () {
 
 
   let arrA = new ArrInputBox('arrA', matirxArrA, 10); //새로운 객체 생성
-  arrA.arrDisplayFunc(arrcont); //화면에 띄우기
+  arrA.arrDisplayFunc(arrcont,arrcont); //화면에 띄우기
 
   let arrB = new ArrInputBox('arrB', matirxArrB, 100); //새로운 객체 생성
-  arrB.arrDisplayFunc(arrcont); //화면에 띄우기
+  arrB.arrDisplayFunc(arrcont,arrcont); //화면에 띄우기
 
   let arrayRS = new ArrResultBox('arrRS', matirxArrR); //새로운 결과 객체 생성
-  arrayRS.arrDisplayFunc(arrcont);
+  arrayRS.arrDisplayFunc(arrcont,arrcont);
 
-  //행렬 더하기 버튼
-  plusBtn.addEventListener("click", function () {
-    if (arrcont == 6) {
-    } else {
-      arrA.arrDisplayFunc(++arrcont); //행렬 늘리기
-      arrB.arrDisplayFunc(arrcont);
-      arrayRS.arrDisplayFunc(arrcont);
-    }
+  //행렬 체크 버튼을 눌렀을때
+  okABtn.addEventListener("click",()=>{
+    let inputARow = document.querySelector("#inputARow").value;
+    let inputAColumns = document.querySelector("#inputAColumns").value;
+    matirxArrR.style.display = 'none';
+    arrayRS.arrDisplayFunc(inputARow,inputAColumns);
+    // console.log(inputARow);
+    arrA.arrDisplayFunc(inputARow,inputAColumns);
   });
-
-  //행렬 빼기 버튼
-  minusBtn.addEventListener("click", function () {
-    if (arrcont == 1) {
-    } else {
-      arrA.arrDisplayFunc(--arrcont); //행렬 줄이기
-      arrB.arrDisplayFunc(arrcont);
-      arrayRS.arrDisplayFunc(arrcont);
-    }
+  okBBtn.addEventListener("click",()=>{
+    let inputBRow = document.querySelector("#inputBRow").value;
+    let inputBColumns = document.querySelector("#inputBColumns").value;
+    matirxArrR.style.display = 'none';
+    // console.log(inputBRow);
+    arrB.arrDisplayFunc(inputBRow,inputBColumns);
+  });
+  //행렬 랜덤 버튼
+  ramdomABtn.addEventListener("click",()=>{
+    let arrAV = document.querySelectorAll(".arrA"); 
+    arrAV.forEach((element,i) => {
+      arrAV[i].value = Math.floor(Math.random()*999);
+    }); 
+  });
+  ramdomBBtn.addEventListener("click",()=>{
+    let arrBV = document.querySelectorAll(".arrB"); 
+    arrBV.forEach((element,i) => {
+      arrBV[i].value = Math.floor(Math.random()*999);
+    }); 
   });
 
   //행렬 더하기 함수
@@ -109,69 +120,136 @@ window.onload = function () {
     return arrResult;
   }
   //행렬 곱하기 함수
-  let mulMatirxFunc = () => {
+  let mulMatirxFunc = (inputARow,inputAColumns,inputBRow,inputBColumns) => {
     let arrAV = document.querySelectorAll(".arrA");
     let arrBV = document.querySelectorAll(".arrB");
+
+    let conutNum = 0;
+
     let arrResult = [];
-    let arrBR = new array(arrcont);
-    let arrAR = new array(arrcont);
+    let arrAR = Array.from(Array(inputARow), ()=> Array(inputAColumns).fill(0));
+    let arrBR = Array.from(Array(inputBRow), ()=> Array(inputBColumns).fill(0));
 
-    for (let i = 0; i < arrBR.length; i++) {
-      arrBR[i] = new Array(arrcont);
+
+    // arrAR.forEach((e,i,a)=>{ //[] 배열을 복사하여 사용하는것이라 값이 변화가 일어나지  x
+    //   a.forEach((e2,i2,a2)=>{ //[[]]
+    //     e2 = arrAV[conutNum].value;
+    //     conutNum++;
+    //   });
+    // });
+
+    conutNum = 0;
+    for(let i = 0; i<arrAR.length; i++){
+      for(let j=0; j<arrAR[i].length;j++ ){
+        arrAR[i][j] = Number(arrAV[conutNum].value);
+        conutNum++; 
+      }
+    }
+    conutNum = 0;
+    for(let i = 0; i<arrBR.length; i++){
+      for(let j=0; j<arrBR[i].length;j++ ){
+        arrBR[i][j] = Number(arrBV[conutNum].value);
+        conutNum++; 
+      }
     }
 
-    for(let i = 0; i < arrcont;i++){
-      arrResult[0] += arrAR[0][i] * arrBR[i][0]; 
-    }
-    //배열 b 쪼개기
-    for(let i=0; i<arrBV.length; i++){
+    console.log(arrAR);
+    // console.log(arrBR);
     
+    conutNum = 0;
+    for(let i = 0; i < inputARow; i++){
+      for(let j = 0; j < inputBColumns; j++){
+        for(let k = 0; k < inputAColumns; k++){
+          arrResult[conutNum] += arrAR[i][k] * arrBR[k][j];
+          console.log(arrResult[conutNum]);
+        }
+        conutNum++;
+      }
     }
 
-    
+    console.log(arrResult);
+
     return arrResult;
   }
 
-  //결과창 팝업
-  xBtn.addEventListener('click', function () {
-    resultBox.style.display = "none";
-  });
 
-  // 하단 더하기 버튼을 누르면
+  // 사이드 더하기 버튼을 누르면
   addBtn.addEventListener("click", function () {
-    let arrRS = document.querySelectorAll("#arrRS");
-    let resultValue = addMatirxFunc();
-
-
-    resultBox.style.display = "block";
-    resultValue.forEach((element, index) => { 
-      arrRS[index].value = element; 
-    });
+    let inputARow = document.querySelector("#inputARow").value;
+    let inputAColumns = document.querySelector("#inputAColumns").value;
+    let inputBRow = document.querySelector("#inputBRow").value;
+    let inputBColumns = document.querySelector("#inputBColumns").value;
+    
+    if(inputARow == inputBRow && inputAColumns == inputBColumns){
+      let arrRS = document.querySelectorAll("#arrRS");
+      let resultValue = addMatirxFunc();
+  
+      matirxArrR.style.display = 'inline-block';
+  
+      resultValue.forEach((element, index) => { 
+        arrRS[index].value = element; 
+      });
+    }else{
+      blackBox.innerHTML = "<p>더하기는 행과 열이 같아야 계산이 가능합니다.</p>";
+      blackBox.style.display = "block";
+      setTimeout(function(){
+        blackBox.style.display = "none"; 
+      },2000);
+    }
+    
   });
 
-  //하단 뺴기 버튼을 누르면
+  //사이드 뺴기 버튼을 누르면
   miusBtn.addEventListener("click", function () {
-    let arrRS = document.querySelectorAll("#arrRS");
-    let resultValue = miusMatirxFunc();
-    let i = 0;
-
-    resultBox.style.display = "block";
-
-    resultValue.forEach((element, index) => { 
-      arrRS[index].value = element; 
-    });
+    let inputARow = document.querySelector("#inputARow").value;
+    let inputAColumns = document.querySelector("#inputAColumns").value;
+    let inputBRow = document.querySelector("#inputBRow").value;
+    let inputBColumns = document.querySelector("#inputBColumns").value;
+    
+    if(inputARow == inputBRow && inputAColumns == inputBColumns){
+      let arrRS = document.querySelectorAll("#arrRS");
+      let resultValue = miusMatirxFunc();
+      let i = 0;
+  
+      matirxArrR.style.display = 'inline-block';
+  
+      resultValue.forEach((element, index) => { 
+        arrRS[index].value = element; 
+      });
+    }else{
+      blackBox.innerHTML = "<p>빼기는 행과 열이 같아야 계산이 가능합니다.</p>";
+      blackBox.style.display = "block";
+      setTimeout(function(){
+        blackBox.style.display = "none"; 
+      },2000);
+    }
   });
 
-  //하단 곱하기 버튼을 누르면
+  //사이드 곱하기 버튼을 누르면
   mulBtn.addEventListener("click",function(){
-    let arrRS = document.querySelectorAll("#arrRS");
-    let resultValue = mulMatirxFunc();
+    let inputARow = Number(document.querySelector("#inputARow").value);
+    let inputAColumns = Number(document.querySelector("#inputAColumns").value);
+    let inputBRow = Number(document.querySelector("#inputBRow").value);
+    let inputBColumns = Number(document.querySelector("#inputBColumns").value);
+    
+    arrayRS.arrDisplayFunc(inputBRow,inputAColumns);
+    if(inputARow == inputBColumns){
 
-    resultBox.style.display = "block";
+      let arrRS = document.querySelectorAll("#arrRS");
+      let resultValue = mulMatirxFunc(inputARow,inputAColumns,inputBRow,inputBColumns);
 
-    resultValue.forEach((element, index) => { 
-      arrRS[index].value = element; 
-    });
+      matirxArrR.style.display = 'inline-block';
+
+      resultValue.forEach((element, index) => { 
+        arrRS[index].value = element; 
+      });
+    }else{
+      blackBox.innerHTML = "<p>곱하기는 앞행 = 뒷열이 같아야 계산이 가능합니다.</p>";
+      blackBox.style.display = "block";
+      setTimeout(function(){
+        blackBox.style.display = "none"; 
+      },2000);
+    }
   });
 
 }
