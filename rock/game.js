8e80d55d1c35fbc6236350ed8a8642e513788f08
cats0713 <cats0713@naver.com>
2022-03-09 01:40:21 +0900

    가위바위보 게임 디자인 수정 및 게임방식 개선

--- rock/game.js
+++ rock/game.js
@@ -1,89 +1,100 @@
 window.onload = () => {
+  let coincount = 0;
+
+  $("#userCoin").on("click", function () {
+    $("#winOrLose").attr("src", './IMG/blank.png');
+    if (coincount < 99) {
+      coincount++;
+      $(".userHeart").css("display", 'block');
+      $("#userHeart").html(`x ${coincount}`);
+    }
+  });
+
   let userRock = document.querySelector("#userRock");
-    let userPaper = document.querySelector("#userPaper");
-    let userScissors = document.querySelector("#userScissors");
+  let userPaper = document.querySelector("#userPaper");
+  let userScissors = document.querySelector("#userScissors");
 
-    let winOrLose = document.querySelector("#winOrLose");
-    let comHand = document.querySelector("#comHand");
+  let winOrLose = document.querySelector("#winOrLose");
+  let comHand = document.querySelector("#comHand");
 
-    function comVsUser() { // 컴퓨터 랜덤 함수를 가위바위보중 하나로 바꾸는 작업
-      let comGame = Math.floor(Math.random() * 3);
-      if (comGame == 0) {
-        comGame = "rock"
-      } else if (comGame == 1) {
-        comGame = 'paper';
-      } else {
-        comGame = 'scissor';
+  function comVsUser() { // 컴퓨터 랜덤 함수를 가위바위보중 하나로 바꾸는 작업
+    let comGame = Math.floor(Math.random() * 3);
+    if (comGame == 0) {
+      comGame = "rock"
+    } else if (comGame == 1) {
+      comGame = 'paper';
+    } else {
+      comGame = 'scissor';
+    }
+    return comGame;
+  }
+
+  function gameResult(gameR) { //게임 결과를 출력
+    if (gameR == 0) {
+      winOrLose.setAttribute('src', './IMG/win.png');
+      winOrLose.setAttribute('alt', '사용자가 이겼다');
+    } else if (gameR == 1) {
+      coincount--;
+      $("#userHeart").html(`x ${coincount}`);
+      if (coincount <= 0) {
+        $("#winOrLose").attr("src", './IMG/star.png');
       }
-      return comGame;
+      // winOrLose.setAttribute('src', './IMG/lose.png');
+      // winOrLose.setAttribute('alt', '사용자가 졌다');
+    } else {
+      winOrLose.setAttribute('src', './IMG/tie.png');
+      winOrLose.setAttribute('alt', '비겼다');
+    }
+  }
+
+  let comResultImg = (comGameResult) => {
+    if (comGameResult == 'rock') {
+      comHand.setAttribute('src', './IMG/rock.png');
+    } else if (comGameResult == 'paper') {
+      comHand.setAttribute('src', './IMG/paper.png');
+    } else {
+      comHand.setAttribute('src', './IMG/scissors.png');
     }
 
-    function gameResult(gameR) { //게임 결과를 출력
-      if (gameR == 0) {
-        winOrLose.setAttribute('src','./IMG/win.png');
-        winOrLose.setAttribute('alt','사용자가 이겼다');
-      } else if (gameR == 1) {
-        winOrLose.setAttribute('src','./IMG/lose.png');
-        winOrLose.setAttribute('alt','사용자가 졌다');
-      } else {
-        winOrLose.setAttribute('src','./IMG/tie.png');
-        winOrLose.setAttribute('alt','비겼다');
-      }
-    }
+  }
 
-    let comResultImg = (comGameResult) => {
-      if (comGameResult == 'rock') {
-        comHand.setAttribute('src','./IMG/rock.png');
-      } else if (comGameResult == 'paper') {
-        comHand.setAttribute('src','./IMG/paper.png');
-      } else {
-        comHand.setAttribute('src','./IMG/scissors.png');
-      }
+  // button을 눌렀을때
+  userRock.addEventListener("click", function () {
+    let comGameResult = comVsUser();
+    if (comGameResult == 'rock') {
+      gameResult(3);
+    } else if (comGameResult == 'paper') {
+      gameResult(1);
+    } else {
+      gameResult(0);
+    }
+    comResultImg(comGameResult);
 
-    }
-    
-    $(".hov-anim").mouseover(function() {
-      $(this).attr("src", $(this).data("animated"))
-    }),
-    $(".hov-anim").mouseout(function() {
-      $(this).attr("src", $(this).data("static"))
-    });
-    
-    // button을 눌렀을때
-    userRock.onclick = function () { 
-      let comGameResult = comVsUser();
-      if (comGameResult == 'rock') {
-        gameResult(3);
-      } else if (comGameResult == 'paper') {
-        gameResult(1);
-      } else {
-        gameResult(0);
-      }
-      comResultImg(comGameResult);
-      
-    }
+  });
 
-    userPaper.onclick = function () {
-      let comGameResult = comVsUser();
-      if (comGameResult == 'rock') {
-        gameResult(0);
-      } else if (comGameResult == 'paper') {
-        gameResult(3);
-      } else {
-        gameResult(1);
-      }
-      comResultImg(comGameResult);
+  userPaper.addEventListener("click", function () {
+    let comGameResult = comVsUser();
+    console.log("a");
+    if (comGameResult == 'rock') {
+      gameResult(0);
+    } else if (comGameResult == 'paper') {
+      gameResult(3);
+    } else {
+      gameResult(1);
     }
+    comResultImg(comGameResult);
+  });
 
-    userScissors.onclick = function () {
-      let comGameResult = comVsUser();
-      if (comGameResult == 'rock') {
-        gameResult(1);
-      } else if (comGameResult == 'paper') {
-        gameResult(0);
-      } else {
-        gameResult(3);
-      }
-      comResultImg(comGameResult);
+  userScissors.addEventListener("click", function () {
+    let comGameResult = comVsUser();
+    if (comGameResult == 'rock') {
+      gameResult(1);
+    } else if (comGameResult == 'paper') {
+      gameResult(0);
+    } else {
+      gameResult(3);
     }
+    comResultImg(comGameResult);
+  });
+  
 }
