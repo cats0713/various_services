caca22b50131b3aeceadbd793ac45cebf51b1034
cats0713 <cats0713@naver.com>
2022-03-16 00:27:54 +0900

    nodejs 파일쪼개기,  pug 랜더링 공부

--- forec/index.js
+++ forec/index.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const app = express();
+const cookieParser = require('cookie-parser');
 
 app.locals.pretty = true;
 app.set('view engine', 'pug');
-app.set('views','./pugfile');
-app.use(express.static('IMG'));
+app.set('views', './pugfile');
+app.use(express.static('FILE'));
+app.use(cookieParser());
 
-app.get('/',(req,res)=>{
-  res.render('intro/selectMoive.pug');
+app.get('/',(req,res) => {
+  let page = req.query.page;
+  console.log(page);
+  res.render('intro/selectMoive.pug',{"page": page});
 });
-app.get('/cat1',(req,res)=>{
-  res.render('2.pug');
-});
-// app.get('/cat2',(req,res)=>{
-//   res.render('img',{'tilte': 'cat2' , 'time':'2022-05-13', 'imgSrc' : 'cat4.jpg', 'text':'yu'});
-// });
-app.listen(2000,()=>{
+
+app.listen(2000,() => {
   console.log("2000 open");
 });
--- forec/sql/movie.sql
+++ forec/sql/movie.sql
@@ -2,6 +2,8 @@ create table `movie`(
   `no` tinyint not null auto_increment,
   `name` char(100) not null,
   `genre` char(10) not null,
-  `time` char(10) not null,
-  primary key `no`,
+  `age` int(30) not null,
+  `star` int(5)not null,
+  `img` char(100) not null, 
+  primary key `no`
 );
